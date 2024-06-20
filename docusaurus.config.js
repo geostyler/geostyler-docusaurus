5617a5c242c14bf363f310313c4731db4a863729
Sylvain Lafay <sylvain.lafay@ign.fr>
2024-06-20 11:01:23 +0200

    fix: change baseUrl and project name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,12 +13,12 @@ const config = {
   url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/geostyler-site',
+  baseUrl: '/geostyler-docusaurus',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'geostyler', // Usually your GitHub org/user name.
-  projectName: 'geostyler-site', // Usually your repo name.
+  projectName: 'geostyler-docusaurus', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
